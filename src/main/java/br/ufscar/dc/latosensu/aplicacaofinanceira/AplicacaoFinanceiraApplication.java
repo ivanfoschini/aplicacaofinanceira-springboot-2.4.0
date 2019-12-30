9407a598b03149aada811075830e9ab2b3235b29
Ivan Joao Foschini <ivanfoschini@bol.com.br>
2019-12-30 12:44:55 -0300

    Commit para o Heroku.

--- src/main/java/br/ufscar/dc/latosensu/aplicacaofinanceira/AplicacaoFinanceiraApplication.java
+++ src/main/java/br/ufscar/dc/latosensu/aplicacaofinanceira/AplicacaoFinanceiraApplication.java
@@ -2,11 +2,15 @@ package br.ufscar.dc.latosensu.aplicacaofinanceira;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
-public class AplicacaoFinanceiraApplication {
+@ComponentScan
+public class AplicacaoFinanceiraApplication extends SpringBootServletInitializer {
 
     @Bean
     public ResourceBundleMessageSource messageSource() {
@@ -14,6 +18,11 @@ public class AplicacaoFinanceiraApplication {
         resourceBundleMessageSource.setBasenames("ValidationMessages", "i18n/messages");
         resourceBundleMessageSource.setUseCodeAsDefaultMessage(true);
         return resourceBundleMessageSource;
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AplicacaoFinanceiraApplication.class);
     }
 
     public static void main(String[] args) {
