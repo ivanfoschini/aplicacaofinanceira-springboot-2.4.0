b5370cadf4d5cd932b703c04e13cab3a36e3de34
Ivan Joao Foschini <ivanfoschini@bol.com.br>
2019-12-31 13:17:17 -0300

    Commit para o Heroku.

--- src/main/java/br/ufscar/dc/latosensu/aplicacaofinanceira/AplicacaoFinanceiraApplication.java
+++ src/main/java/br/ufscar/dc/latosensu/aplicacaofinanceira/AplicacaoFinanceiraApplication.java
@@ -2,15 +2,11 @@ package br.ufscar.dc.latosensu.aplicacaofinanceira;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
-@ComponentScan
-public class AplicacaoFinanceiraApplication extends SpringBootServletInitializer {
+public class AplicacaoFinanceiraApplication {
 
     @Bean
     public ResourceBundleMessageSource messageSource() {
@@ -20,11 +16,6 @@ public class AplicacaoFinanceiraApplication extends SpringBootServletInitializer
         return resourceBundleMessageSource;
     }
     
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(AplicacaoFinanceiraApplication.class);
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(AplicacaoFinanceiraApplication.class, args);
     }
